da4c599d8215695dad6227812521ae8827082612
AlanPinheiro96 <alantnt15@gmail.com>
2025-02-14 19:28:52 -0300

    Atualizações Models

--- EagleStore/Models/Produto.cs
+++ EagleStore/Models/Produto.cs
@@ -6,13 +6,22 @@ namespace EagleStore.Models;
 [Table("Produto")]
 public class Produto
 {
+    [Key]
     public int Id { get; set; }
 
-    public Categoria CategoriaId { get; set; }
+    [Required(ErrorMessage = "Por favor, informe a categoria")]
+    public int CategoriaId { get; set; }
+    [ForeignKey(nameof(CategoriaId))]
+    public Categoria Categoria { get; set; }
 
+    [Required(ErrorMessage = "Por favor, informe o nome")]
+    [StringLength(60, ErrorMessage = "O nome deve possuir no máximo 60 caracteres")]
     public string Nome { get; set; }
 
+    [Display(Name = "Descrição")]
+    [StringLength(1000, ErrorMessage = "O nome deve possuir no máximo 1000 caracteres")]
     public string Descricao { get; set; }
+
 
     public int Qtde { get; set; }
 
